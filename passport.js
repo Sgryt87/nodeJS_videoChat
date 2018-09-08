242892b4e109e8ad88261feb431506ec7800038c
Sergii <sergiigrytsaienko@gmail.com>
2018-09-08 16:22:34 -0500

    authentication with facebook is added

--- passport.js
+++ passport.js
@@ -1,6 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
+var FacebookStrategy = require('passport-facebook').Strategy;
 passport.serializeUser(function (user, done) {
     done(null, user._id);
 });
@@ -30,6 +30,48 @@ passport.use(new LocalStrategy({
                 })
             }
             return done(null, user);
+        });
+    })
+);
+
+passport.use(new FacebookStrategy({
+        clientID: 'xxx',
+        clientSecret: 'zzz',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback',
+        profileFields: ['id', 'displayName', 'email']
+    }, function (token, refreshToken, profile, done) {
+        User.findOne({'facebookId': profile.id}, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                User.findOne({email: profile.emails[0].value}, function (err, user) {
+                    if (user) {
+                        user.facebookId = profile.id;
+                        return user.save(function (err) {
+                            if (err) {
+                                return done(null, false, {message: 'Cant save user info'});
+                            }
+                            return done(null, user);
+                        })
+                    }
+
+                    var user = new User();
+                    user.name = profile.displayName;
+                    user.email = profile.emails[0].value;
+                    user.facebookId = profile.id;
+                    user.save(function (err) {
+                        if (err) {
+                            return done(null, false, {message: 'Cant save user info'});
+                        }
+                        return done(null, user);
+                    });
+
+
+                });
+            }
         });
     })
 );
